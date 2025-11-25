884988ff9e9f8d267b7b8850c6a5181da5aed296
pabartoc <pabartoc@student.42heilbronn.de>
2025-11-25 12:09:53 +0100

    updated: the header file get_next_line.h with new includes

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,22 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 23:52:00 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/11/22 23:57:33 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/11/25 12:02:51 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h> // for read, write, close
+# include <stdlib.h> // for malloc, free
+# include <stddef.h> // for size_t
+# include <fcntl.h>  // for open and flags (optional)
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+#endif
+
+char	*get_next_line(int fd);
+
+#endif
