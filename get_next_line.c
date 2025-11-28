ee45c6b178e66614250c21187adfb5486c760004
pabartoc <pabartoc@student.42heilbronn.de>
2025-11-28 18:32:12 +0100

    updated: Moved the functions strlen_gnl and strchr_gnl to the file get_next_line_utils.c. Declared two variables: remainder & *buffer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 23:51:34 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/11/25 18:15:08 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/11/28 18:18:43 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,11 @@
 
 char	*get_next_line(int fd)
 {
-	
-}
+	static char	remainder[BUFFER_SIZE + 1];
+	char		*buffer;
 
-size_t	strlen_gnl(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-char	*strchr_gnl(const char *str, int c)
-{
-	size_t	i;
-
-	i = 0;
-	if (!str)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	while (str[i] != '\0')
-	{
-		if ((unsigned char) str[i] == (unsigned char) c)
-			return ((char *) &str[i]);
-		i++;
-	}
-	if ((unsigned char) str[i] == (unsigned char) c)
-		return ((char *) &str[i]);
-	return (NULL);
 }
+
 
