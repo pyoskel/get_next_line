3885a5db8370d42d06f86490444cbd6e50ad5f96
pabartoc <pabartoc@student.42heilbronn.de>
2025-11-28 18:33:39 +0100

    implement: Add strlen_gnl and strchr_gnl functions to get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,38 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/22 23:54:08 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/11/22 23:54:14 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/11/28 18:16:37 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+size_t	strlen_gnl(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*strchr_gnl(const char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		if ((unsigned char) str[i] == (unsigned char) c)
+			return ((char *) &str[i]);
+		i++;
+	}
+	if ((unsigned char) str[i] == (unsigned char) c)
+		return ((char *) &str[i]);
+	return (NULL);
+}
